03e4e59a9cf8ac70c2a155f3742c53dccce07a03
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2025-09-05 11:11:24 +0100

    update addon.service.ts

--- src/application/services/addon.service.ts
+++ src/application/services/addon.service.ts
@@ -1,8 +1,8 @@
 // src/application/services/brand.service.ts
 import { Injectable } from '@nestjs/common';
 import { BaseService } from './base.service';
-import { AddonEntity } from 'src/domain/entities/addon.entity';
-import { AddonRepository } from 'src/domain/repositories/addon.reposItory';
+import { AddonEntity } from '../../domain/entities/addon.entity';
+import { AddonRepository } from '../../domain/repositories/addon.reposItory';
 
 @Injectable()
 export class AddonService extends BaseService<AddonEntity> {
