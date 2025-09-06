d459f12c5a42b231d505e81c52503f7fe5982430
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2025-09-06 19:47:17 +0100

    build(deps): update package-lock.json, package.json, jwt.strategy.ts and main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Order Processing API')
+    .setDescription('API documentation for the Order Processing System')
+    .setVersion('1.0')
+    .addBearerAuth() // Add JWT authentication option
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 
